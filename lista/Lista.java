556852c596583184495ba2f5c0795d6419ad5bb4
EduarCeballos <L23170037@culiacan.tecnm.mx>
2024-10-04 15:02:24 +0000

    continuacion de lista

--- lista/Lista.java
+++ lista/Lista.java
@@ -15,14 +15,37 @@ public class Lista<T>{
         return tamanio;
     }
     public boolean esVacia(){
-        if (cabeza == null)
-        return true;
+        if (cabeza == null);
+       return true;
         else
         return false;
         //return (cabeza==null); otra forma de hacerlo
     }
     public void agregar(T valor){
         Nodo<T> nuevo = new Nodo<T>();
-        
-    }
+        nuevo.setValor(valor);
+        if (!esVacia())
+        cabeza = nuevo;
+        else{
+            //agregar al final de la lista
+            Nodo<T> aux = cabeza;
+            while (aux.getSiguiente()!=null) {
+                aux= aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevo);
+        }
+        tamanio++;
+        /**
+         * insertar un nuevo nodo en la lista
+         * @parram valor a agregar
+         * @param posicion donde se insertara el nodo
+         */
+}
+        public void insertar(T valor, int pos)throws PosicionIlegalException{
+            if(pos>=0 && pos<=tamanio){
+
+            }else {
+                throw new PosicionIlegalException();
+            }  
+}
 }
---
+++ lista/PosicionIlegalException.java
@@ -0,0 +1,9 @@
+package lista;
+
+public class PosicionIlegalException extends Exception {
+    public PosicionIlegalException(){
+        super("posicion ilegal en la lista");
+
+    }
+    
+}
